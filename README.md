6f12a230d6f5569af1bd008806680f13708f634d
iamilyasedunov <iliasedunov@yandex.ru>
2020-05-31 00:12:48 +0300

    final report and exchange implementation

--- README.md
+++ README.md
@@ -7,3 +7,50 @@ The research work on **Cell Formation** algorithms solving *manufacturing cell f
  - [A simulated annealing algorithm for manufacturing cell formation problems](https://ir.nctu.edu.tw/bitstream/11536/9500/1/000253183700003.pdf)<br>
  - [A water flow-like algorithm for manufacturing cell formation problems
 ](https://www.researchgate.net/publication/46491162_A_water_flow-like_algorithm_for_manufacturing_cell_formation_problems) <br>
+
+**Седунов, Альперович**
+
+  --- 
+
+***benchmarks:*** ```./data```
+ 
+- initial temperature ($T_f$) : $10$ 
+- final temperature ($T_0$)   : $0.002$
+- chain len ($L$)             : $4$
+- len of period ($D$)         : $6$
+- numb of cells ($C_0$)       : $2$
+- check                       : $5$
+- cooling rate ($\alpha$)     : $0.7$
+
+--- 
+
+## Benchmark 20x20
+- $f_{start}$   : ```0.269```, $f_{best}$ : ```0.380```
+- $C_{optimal}$ : ```3```
+- $t_{end}$     : ```0.0027```
+- $time$        : ```0:00:55```
+--- 
+## Benchmark 24x40
+- $f_{start}$   : ```0.147```, $f_{best}$ : ```0.278```
+- $C_{optimal}$ : ```4```
+- $t_{end}$     : ```0.0019```
+- $time$        : ```0:01:35```
+--- 
+## Benchmark 30x50
+- $f_{start}$   : ```0.140```, $f_{best}$ : ```0.277```
+- $C_{optimal}$ : ```6```
+- $t_{end}$     : ```0.0019```
+- $time$        : ```0:01:35```
+--- 
+## Benchmark 30x90
+- $f_{start}$   : ```0.108```, $f_{best}$ : ```0.257```
+- $C_{optimal}$ : ```5```
+- $t_{end}$     : ```0.0019```
+- $time$        : ```0:02:39```
+--- 
+## Benchmark 37x53
+- $f_{start}$   : ```0.437```, $f_{best}$ : ```0.449```
+- $C_{optimal}$ : ```2```
+- $t_{end}$     : ```0.0969```
+- $time$        : ```0:00:51```
+--- 
--- algorithm/SimulatedAnnealing.py
+++ algorithm/SimulatedAnnealing.py
@@ -15,9 +15,10 @@ class SimulatedAnnealing(Algorithm):
         self.history = []
         self.f = {'current' : 0, 'neigh' : 0, 'best' : 0}
         # solution
+        self.last_cell_nums = [] 
         self.S = {'current' :  {'p' : {}, 'm' : {}},
                   'neigh': {'p' : {}, 'm' : {}},
-                  'best' : {'p' : {}, 'm' : {}},
+                  'neigh_curr' : {'p' : {}, 'm' : {}},
                   'best_so_far' : {'p' : {}, 'm' : {}},
                   'initial' : {'p' : {}, 'm' : {}}}
 
@@ -36,10 +37,9 @@ class SimulatedAnnealing(Algorithm):
 
     def solve(self):
         flag = True
-        delta = 0
-
-
+        delta, counter = 0, 0
         while (self.c['MC'] < self.L and self.c['trapped'] < self.L/2):
+            counter+=1
             if flag:
                 # generate initial solution by
                 # using parts assignment and machines assignment
@@ -48,17 +48,17 @@ class SimulatedAnnealing(Algorithm):
 
                 self.f['current'] = self.get_grouping_efficacy('initial')
                 flag = False
-            
+            if counter == 1:
+                self.print_params()
+
             self.single_move()
-            print('begin')
-            self.print_params()
-            #print(self.S['current']['p'], self.S['neigh']['p'])
+            
+            if counter % self.D == 0:
+                self.exchange_move()
             self.S['neigh']['m'] = tools.get_asignments_by_machine(self.machine_part_matrix, self.S['neigh']['p'])
             if self.f['current'] < self.f['neigh']:
-                #self.S['best'] = copy.deepcopy(self.S['neigh'])
                 self.S['current'] = copy.deepcopy(self.S['neigh'])
                 self.f['current'] = copy.deepcopy(self.f['neigh'])
-                #self.f['best'] = copy.deepcopy(self.f['neigh'])
                 self.c['stagnant'] = 0
             elif self.f['current'] == self.f['neigh']:
                 self.c['stagnant'] += 1
@@ -70,7 +70,7 @@ class SimulatedAnnealing(Algorithm):
                     self.c['trapped'] = 0
                 else:
                     self.c['trapped'] += 1
-            if self.t['current'] > self.t['final'] or self.c['stagnant'] >= self.c['check']:
+            if self.t['current'] < self.t['final'] or self.c['stagnant'] >= self.c['check']:
                 if self.f['current'] > self.f['best']:
                     self.S['best'] = copy.deepcopy(self.S['current'])
                     self.f['best'] = copy.deepcopy(self.f['current'])
@@ -78,26 +78,25 @@ class SimulatedAnnealing(Algorithm):
                     self.n['initial'] += 1
                     flag = True
             else:
-                self.t['current'] = self.t['current'] * self.cooling_rate
+                self.t['current'] = copy.deepcopy(self.t['current'] * self.cooling_rate)
                 self.c['MC'] = 0
             self.c['MC'] += 1
-            print('end')
-            self.print_params()
         self.print_params()
+
     def get_grouping_efficacy(self, type_efficacy):
         return tools.compute_grouping_efficacy(self.S[type_efficacy]['p'], self.S[type_efficacy]['m'],                                                                             tools.solution_df(self.S[type_efficacy]['p'], 
-                                            self.S[type_efficacy]['m'], 
-                                            self.machine_part_matrix))
-            
+                                      self.S[type_efficacy]['m'], 
+                                      self.machine_part_matrix))
+
     def print_params(self):
         print("              f: {}".format(self.f))
-        #print("solution       : {}\n {}".format(self.S['neigh']['p'], self.S['current']['p']))
+        # print("solution       : {}".format(self.S['neigh']['p']))
         print("temperature    : {}".format(self.t))
-        #print("cooling_rate   : {}".format(self.cooling_rate))
-        #print("L: {},        D : {}".format(self.L, self.D))
-        print("number of cells: {}\n\n".format(self.n))
-
+        #print("last cell: {},     D : {}".format(set(self.last_cell_nums), self.D))
+        print("number of cells: {} cool_rate: {}\n\n".format(self.n, self.cooling_rate))
+        
     def single_move(self):
+        self.last_cell_nums = []
         for ind_source, cell_source in enumerate(list(self.S['current']['p'].values())):
             if len(cell_source) == 1:
                 continue
@@ -112,48 +111,34 @@ class SimulatedAnnealing(Algorithm):
                     #print("current: {}\n, f: {}".format(
                     #        self.S['neigh']['p'], 
                     #        self.f))
-                    curr_f = tools.compute_grouping_efficacy(self.S['current']['p'], self.S['current']['m'],                                                                             tools.solution_df(self.S['neigh']['p'], 
+                    curr_f = tools.compute_grouping_efficacy(self.S['current']['p'], self.S['current']['m'],                                                                             tools.solution_df(self.S['current']['p'], 
                                         self.S['current']['m'], 
                                         self.machine_part_matrix))
                     if curr_f > self.f['current']:
+                        self.last_cell_nums.append((ind_source, ind_dest, j))
                         self.S['neigh'] = copy.deepcopy(self.S['neigh'])
                         self.f['neigh'] = copy.deepcopy(curr_f)
                     self.S['current']['p'][ind_dest].remove(j)
                 self.S['current']['p'][ind_source].append(j)
-
-            # self.single_move()
-            
-            # self.S['neigh']['m'] = tools.get_asignments_by_machine(self.machine_part_matrix, self.S['neigh']['p'])
-            # print('0 ', self.f, self.S['current']['p'], self.S['neigh']['p'])
-
-            # if (self.get_grouping_efficacy('neigh') > self.get_grouping_efficacy('best')):
-                # self.S['best'] = copy.deepcopy(self.S['neigh'])
-                # self.S['current'] = copy.deepcopy(self.S['neigh'])
-                # self.c['stagnant'] = 0
-                # self.c['MC'] += 1
-                # print('1 ', self.f)
-            # elif (self.get_grouping_efficacy('neigh') == self.get_grouping_efficacy('best')):
-                # print('2 ', self.f)
-
-                # self.S['curr'] = copy.deepcopy(self.S['neigh'])
-                # self.c['stagnant'] += 1
-                # self.c['MC'] += 1
-            # delta = self.get_grouping_efficacy('current') - self.get_grouping_efficacy('neigh')
-            # rand = random.random()
-            # if (math.exp(delta/self.t['curr'])):
-                # self.S['current'] = copy.deepcopy(self.S['neigh'])
-                # self.c['trapped'] = 0
-            # else:
-                # self.c['trapped'] += 1
-            # print('3 ', self.f)
-
-            # if self.t['curr'] <= self.t['final'] or self.c['stagnant'] >= self.c['check']:
-                # if self.get_grouping_efficacy('best') > self.get_grouping_efficacy('best_so_far'):
-                    # self.S['best_so_far'] = copy.deepcopy(self.S['best'])
-                    # self.n['optimal'] = copy.deepcopy(self.n['initial'])
-                    # self.n['initial'] += 1
-                    # flag = True
-                # else:
-                    # self.t['curr'] = self.t['curr'] * self.cooling_rate
-                    # self.c['MC'] = 0
-            # self.c['MC']+=1
+    
+    def exchange_move(self):
+        solution = {}
+        f_best, curr_f = copy.deepcopy(self.f['neigh']), copy.deepcopy(self.f['neigh'])
+        for history in self.last_cell_nums:
+            ind_source, ind_dest, j = history
+            for part in self.S['neigh']['p'][ind_dest]:
+                if part == j:
+                    continue
+                self.S['neigh']['p'][ind_dest].remove(part)
+                self.S['neigh']['p'][ind_source].append(part)
+                curr_f = tools.compute_grouping_efficacy(self.S['neigh']['p'], self.S['neigh']['m'],                                                                             tools.solution_df(self.S['neigh']['p'], 
+                                        self.S['neigh']['m'], 
+                                        self.machine_part_matrix))
+                if curr_f > f_best:
+                    solution = copy.deepcopy(self.S['neigh'])
+                    f_best = copy.deepcopy(self.f['neigh'])
+                self.S['neigh']['p'][ind_dest].append(part)
+                self.S['neigh']['p'][ind_source].remove(part)
+        if f_best > self.f['neigh']:
+            self.S['neigh'] = copy.deepcopy(solution)
+            self.f['neigh'] = copy.deepcopy(f_best)
